9ba76c4cdd6c62c689579fbf0158918bb2b8a6ee
Jan Dittrich <mail@jand.one>
2022-10-22 19:08:28 +0000

    Bind on every host

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ def main():
     config = Config()
     app = flask.create_app(config)
 
-    app.run(debug=True, ssl_context="adhoc", use_reloader=False)
+    app.run(debug=True, ssl_context="adhoc", use_reloader=False, host="0.0.0.0")
 
 
 if __name__ == "__main__":
